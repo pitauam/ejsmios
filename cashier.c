ca3f14dfd672e0f5ecef32ed59764f1b1de027b3
pitauam <santiago.pita@estudiante.uam.es>
2026-02-05 19:59:24 +0100

    cashier.c

---
+++ cashier.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+    int action;
+
+    printf("Hello! this is a cashier. What would you like to do? ");
+    scanf("%i",&action);
+
+    printf("Action selected: %i\n", action);
+
+
+
+    return 0;
+}
