2e96685c507fb7b8fa4f96647844f11402485ab5
pitauam <santiago.pita@estudiante.uam.es>
2026-02-05 20:21:24 +0100

    ej 1

---
+++ ej1/cashier.h
@@ -0,0 +1,9 @@
+#ifndef CASHIER_H
+#define CASHIER_H
+
+
+
+
+
+
+#endif
---
+++ ej1/types.h
@@ -0,0 +1,22 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+
+typedef enum 
+{
+
+    ERROR = 0,
+    OK = 1
+
+}Status;
+
+typedef enum
+{
+
+    TRUE = 0,
+    FALSE = 1
+}Bool;
+
+
+
+#endif
