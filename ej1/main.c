7ec1864bdd424a754974d96c447b48e55f6989f1
pitauam <santiago.pita@estudiante.uam.es>
2026-02-06 15:08:01 +0100

    funciona

--- ej1/main.c
+++ ej1/main.c
@@ -6,34 +6,51 @@ int main(void)
 {
 
     Cashier *cajero = NULL;
-    double balance = INITIAL; /*por defecto tiene 0*/
     double ingreso = INITIAL;
     double withdraw = INITIAL;
-    int choice = -1; /*lo pongo a -1 por defecto*/
+    int choice = 0; /*lo pongo a -1 por defecto*/
 
     cajero = cashier_create(INITIAL);
 
-    do
+    while (choice != -1)
     {
-        printf("Your balance is: %.2lf€.\nType 1 to deposit money or 2 to withdraw money: ", cashier_get_balance(cajero));
-        scanf("%i", &choice);
-    } while (choice != 1 || choice != 2);
 
-    if (choice == 1)
-    {
-        printf("You have chosen to deposit money. Enter amount to deposit: ");
-        scanf("%lf", &ingreso);
-        cashier_deposit(cajero, ingreso);
+        do
+        {
+            printf("Your balance is: %.2f€.\nType 1 to deposit money, 2 to withdraw money or -1 to exit the cashier: ", cashier_get_balance(cajero));
+            scanf("%i", &choice);
+            printf("\n");
+        } while (choice != 1 && choice != 2 && choice != -1);
+
+        if (choice == 1)
+        {
+            printf("You have chosen to deposit money. Enter amount to deposit: ");
+            scanf("%lf", &ingreso);
+
+            if ((cashier_deposit(cajero, ingreso)) == ERROR)
+            {
+                printf("There was an error while depositing money!\n\n"); /*estoy pensando en si tengo que verificar aqui en el main que el dinero ingresado sea positivo para diferenciar entre error depositando y error porque has intentado meter dinero positivo*/
+            }
+
+            
+        }
+        else if (choice == 2)
+        {
+            printf("You have chosen to withdraw money. Enter amount to withdraw: ");
+            scanf("%lf", &withdraw);
+
+            if ((cashier_withdraw(cajero, withdraw)) == ERROR)
+            {
+                printf("There was an error while withdrawing money!\n\n");
+            }
+
+            
+        }
+        else if (choice == -1)
+        {
+            printf("Final balance: %.2f€.\nExiting cashier...\n\n", cashier_get_balance(cajero));
+        }
     }
-    else if (choice == 2)
-    {
-        printf("You have chosen to withdraw money. Enter amount to withdraw: ");
-        scanf("%lf", &withdraw);
-
-        cashier_withdraw(cajero, withdraw);
-    }
-
-    printf("Your current balance is: %.2lf€.\n", cashier_get_balance(cajero));
 
     cashier_free(cajero);
     return 0; /*todo ok*/
