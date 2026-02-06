7592a02cb7a33e8c7c9f2befe2bea0c7289d5c3f
pitauam <santiago.pita@estudiante.uam.es>
2026-02-06 17:03:50 +0100

    dos funciones

--- ej2/rational.c
+++ ej2/rational.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include "rational.h"
 
-struct _Rational {
+struct _Rational
+{
     int n; /* Numerador */
     int d; /* Denominador */
 };
 
 /* Función auxiliar (privada): Máximo Común Divisor  */
-int gcd(int m, int n) {
+int gcd(int m, int n)
+{
     int r;
-    while (n != 0) {
+    while (n != 0)
+    {
         r = m % n;
         m = n;
         n = r;
@@ -18,14 +21,17 @@ int gcd(int m, int n) {
     return m;
 }
 
-Rational* rational_create(int n, int d) {
+Rational *rational_create(int n, int d)
+{
     Rational *r = NULL;
     int common;
 
-    if (d == 0) return NULL; /* Un denominador no puede ser 0 */
+    if (d == 0)
+        return NULL; /* Un denominador no puede ser 0 */
 
-    r = (Rational*) malloc(sizeof(Rational));
-    if (r == NULL) return NULL;
+    r = (Rational *)malloc(sizeof(Rational));
+    if (r == NULL)
+        return NULL;
 
     /* Simplificamos la fracción al crearla usando gcd [cite: 430] */
     common = gcd(n, d);
@@ -35,12 +41,16 @@ Rational* rational_create(int n, int d) {
     return r;
 }
 
-void rational_free(Rational *r) {
-    if (r) free(r);
+void rational_free(Rational *r)
+{
+    if (r)
+        free(r);
 }
 
-Rational* rational_sum(const Rational *r1, const Rational *r2) {
-    if (!r1 || !r2) return NULL;
+Rational *rational_sum(const Rational *r1, const Rational *r2)
+{
+    if (!r1 || !r2)
+        return NULL;
     /* Suma: (a/b) + (c/d) = (a*d + c*b) / (b*d) */
     return rational_create(r1->n * r2->d + r2->n * r1->d, r1->d * r2->d);
 }
@@ -48,11 +58,37 @@ Rational* rational_sum(const Rational *r1, const Rational *r2) {
 /* --- TU TAREA 1: Implementa aquí rational_prod --- */
 /* Pista: (a/b) * (c/d) = (a*c) / (b*d) */
 
+Rational *rational_prod(const Rational *r1, const Rational *r2)
+{
+    if (!r1 || !r2)
+    {
+        return NULL; /*error*/
+    }
+
+    return rational_create((r1->n * r2->n), (r1->d * r2->d));
+}
+
 /* --- TU TAREA 2: Implementa aquí rational_div --- */
 /* Pista: (a/b) / (c/d) = (a*d) / (b*c) */
 
-Status rational_print(const Rational *r) {
-    if (!r) return ERROR;
+Rational *rational_div(const Rational *r1, const Rational *r2)
+{
+    if (!r1 || !r2)
+    {
+        return NULL; /*error*/
+    }
+
+    return rational_create((r1->n * r2->d), (r1->d * r2->n));
+}
+
+
+
+
+
+Status rational_print(const Rational *r)
+{
+    if (!r)
+        return ERROR;
     printf("%d/%d", r->n, r->d);
     return OK;
 }
